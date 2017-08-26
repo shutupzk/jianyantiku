9e3227d56579fada3261c32353152f042802fea2
ruan <894159898@qq.com>
2017-08-26 11:02:52 +0800

    测试

--- server/index.js
+++ server/index.js
@@ -15,6 +15,7 @@ import addModelsToContext from '../model'
 import authenticate from './authenticate'
 import { GRAPHQL_PORT, MONGO_URL } from '../config'
 
+
 const schema = makeExecutableSchema({ typeDefs, resolvers })
 
 async function startServer() {
