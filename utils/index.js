9170fda264b30bd75da0df6556fe68068ea8a29d
zhakang <956468883@qq.com>
2017-08-26 11:51:17 +0800

    xiugai

--- utils/index.js
+++ utils/index.js
@@ -1,62 +1,37 @@
+import moment from 'moment'
+import crypto from 'crypto'
 
-import moment from 'moment';
-import crypto from 'crypto';
-
-//MD5签名
-export const md5 = (str) => {
-  var md5sum = crypto.createHash('md5');
-  md5sum.update(str);
-  str = md5sum.digest('hex');
-  return str;
-};
-
-export const checkPhoneNumber = (phone) => {
-  const r = /(1([3578][0-9]))\d{8}/;
-  return (r.test(phone) && (phone.length === 11));
+// MD5签名
+export const md5 = str => {
+  var md5sum = crypto.createHash('md5')
+  md5sum.update(str)
+  str = md5sum.digest('hex')
+  return str
 }
 
-export const checkIdCard = (num) => {
-  const re = new RegExp(/^(\d{6})(\d{4})(\d{2})(\d{2})(\d{3})([0-9]|X)$/);
-  let arrSplit = num.match(re);
-  let dtmBirth = new Date(arrSplit[2] + "/" + arrSplit[3] + "/" + arrSplit[4]);
-  let bGoodDay;
-  bGoodDay = (dtmBirth.getFullYear() === Number(arrSplit[2])) && ((dtmBirth.getMonth() + 1) === Number(arrSplit[3])) && (dtmBirth.getDate() === Number(arrSplit[4]));
-  if (!bGoodDay) {
-    return false;
-  } else {
-    let valnum;
-    let arrInt = [7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2];
-    let arrCh = ['1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2'];
-    let nTemp = 0, i;
-    for (i = 0; i < 17; i++) {
-      nTemp += num.substr(i, 1) * arrInt[i];
-    }
-    valnum = arrCh[nTemp % 11];
-    if (valnum !== num.substr(17, 1)) {
-      return false;
-    }
-    return num;
-  }
+export const checkPhoneNumber = phone => {
+  const r = /(1([3578][0-9]))\d{8}/
+  return r.test(phone) && phone.length === 11
 }
 
-export const subBirthday = (certNo) => {
+export const subBirthday = certNo => {
   return certNo.substr(6, 4) + '-' + certNo.substr(10, 2) + '-' + certNo.substr(12, 2)
 }
 
-export const subSex = (certNo) => {
-  return certNo.substr(16, 1) % 2 + ""
+export const subSex = certNo => {
+  return certNo.substr(16, 1) % 2 + ''
 }
 
-export const getAge = (birthDay) => {
+export const getAge = birthDay => {
   return moment().format('YYYY') * 1 - birthDay.substr(0, 4) * 1
 }
 
 export const createTradeNo = () => {
-  let year = (moment().format('YY') * 1 - 15)
+  let year = moment().format('YY') * 1 - 15
   let seconds = moment().format('DDD') * 24 * 60 * 60 + moment().format('HH') * 60 * 60 + moment().format('mm') * 60 + moment().format('ss') * 1
   let order = seconds.toString(36)
   order = secondsFormat(order, 5)
-  let ran = ((Math.random() + '').substr(4, 3) + moment().format('SSS'))
+  let ran = (Math.random() + '').substr(4, 3) + moment().format('SSS')
   let random = (ran * 1).toString(36)
   random = secondsFormat(random, 4)
   order = year.toString(36) + order
