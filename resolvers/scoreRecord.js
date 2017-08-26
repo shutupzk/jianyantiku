11c22a9cc506fbf145e550d322a7f77c5c16bcd9
ruan <894159898@qq.com>
2017-08-26 19:47:31 +0800

    添加积分记录表

---
+++ resolvers/scoreRecord.js
@@ -0,0 +1,41 @@
+const resolvers = {
+  ScoreRecord: {
+    id(scoreRecord) {
+      return scoreRecord._id
+    },
+
+    user(scoreRecord, args, { ScoreRecord }) {
+      return ScoreRecord.user(scoreRecord)
+    },
+
+    scoreType(scoreRecord, args, { ScoreRecord }) {
+      return ScoreRecord.scoreType(scoreRecord)
+    }
+
+  },
+  Query: {
+    scoreRecords(root, { skip, limit }, { ScoreRecord }) {
+      return ScoreRecord.all({ skip, limit })
+    },
+    scoreRecord(root, { id }, { ScoreRecord }) {
+      return ScoreRecord.findOneById(id)
+    }
+  },
+  Mutation: {
+    async createScoreRecord(root, { input }, { ScoreRecord }) {
+      const id = await ScoreRecord.insert(input)
+      return ScoreRecord.findOneById(id)
+    },
+
+    async updateScoreRecord(root, { id, input }, { ScoreRecord }) {
+      await ScoreRecord.updateById(id, input)
+      return ScoreRecord.findOneById(id)
+    },
+
+    removeScoreRecord(root, { id }, { ScoreRecord }) {
+      return ScoreRecord.removeById(id)
+    }
+  }
+}
+
+export default resolvers
