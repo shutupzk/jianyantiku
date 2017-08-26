0bdfb6c1072ab73881c693ecf05771f468ce57b3
田先耀 <xianyao9958@163.com>
2017-08-26 16:45:41 +0800

    我的课程

---
+++ model/MyCourse.js
@@ -0,0 +1,51 @@
+import DataLoader from 'dataloader'
+import findByIds from 'mongo-find-by-ids'
+
+export default class MyCourse {
+  constructor(context) {
+    this.context = context
+    this.collection = context.db.collection('myCourse')
+    this.loader = new DataLoader(ids => findByIds(this.collection, ids))
+  }
+
+  findOneById(id) {
+    return this.loader.load(id)
+  }
+
+  all({ skip = 0, limit = 10 }) {
+    return this.collection.find().sort({ createdAt: -1 }).skip(skip).limit(limit).toArray()
+  }
+
+  user(myCourse) {
+    return this.context.User.findOneById(myCourse.userId)
+  }
+
+  course(myCourse) {
+    return this.context.Course.findOneById(myCourse.courseId)
+  }
+
+  async insert(doc) {
+    const docToInsert = Object.assign({}, doc, {
+      createdAt: Date.now(),
+      updatedAt: Date.now()
+    })
+    const id = (await this.collection.insertOne(docToInsert)).insertedId
+    return id
+  }
+
+  async updateById(id, doc) {
+    const ret = await this.collection.update({ _id: id }, {
+      $set: Object.assign({}, doc, {
+        updatedAt: Date.now()
+      })
+    })
+    this.loader.clear(id)
+    return ret
+  }
+
+  async removeById(id) {
+    const ret = this.collection.remove({ _id: id })
+    this.loader.clear(id)
+    return ret
+  }
+}
