87b44d1da73c11a64f2e9339fc4e1e347304587c
zhakang <956468883@qq.com>
2017-08-27 19:59:06 +0800

    xiugai

--- seed/index.js
+++ seed/index.js
@@ -45,7 +45,6 @@ function insertData(db, fn) {
 function insertOneCollection(db, fileName, fn) {
   let docs = require('./data/' + fileName)
   let name = fileName.replace('.json', '')
-  console.log('docs === ', docs)
   if (!docs || !docs.length || docs.length === 0) return fn()
   console.log('create collectios : ' + name)
   for (let i = 0; i < docs.length; i++) {
