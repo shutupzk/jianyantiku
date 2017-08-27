a38b4990785f466ba40a7d409ff3212b8f745d0e
田先耀 <xianyao9958@163.com>
2017-08-27 11:06:07 +0800

    试卷加题的关联表

---
+++ resolvers/examinationHasExercise.js
@@ -0,0 +1,40 @@
+const resolvers = {
+  ExaminationHasExercise: {
+    id(examinationHasExercise) {
+      return examinationHasExercise._id
+    },
+
+    examination(examinationHasExercise, args, { ExaminationHasExercise }) {
+      return ExaminationHasExercise.examination(examinationHasExercise)
+    },
+
+    exercise(examinationHasExercise, args, { ExaminationHasExercise }) {
+      return ExaminationHasExercise.exercise(examinationHasExercise)
+    }
+  },
+  Query: {
+    examinationHasExercises(root, { skip, limit }, { ExaminationHasExercise }) {
+      return ExaminationHasExercise.all({ skip, limit })
+    },
+    examinationHasExercise(root, { id }, { ExaminationHasExercise }) {
+      return ExaminationHasExercise.findOneById(id)
+    }
+  },
+  Mutation: {
+    async createExaminationHasExercise(root, { input }, { ExaminationHasExercise }) {
+      const id = await ExaminationHasExercise.insert(input)
+      return ExaminationHasExercise.findOneById(id)
+    },
+
+    async updateExaminationHasExercise(root, { id, input }, { ExaminationHasExercise }) {
+      await ExaminationHasExercise.updateById(id, input)
+      return ExaminationHasExercise.findOneById(id)
+    },
+
+    removeExaminationHasExercise(root, { id }, { ExaminationHasExercise }) {
+      return ExaminationHasExercise.removeById(id)
+    }
+  }
+}
+
+export default resolvers
