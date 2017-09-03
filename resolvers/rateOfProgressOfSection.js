e22760022af9d0f5d064806a5943ab8fdb390469
康  查 <956468883@qq.com>
2017-09-03 19:41:55 +0800

    xiugai

---
+++ resolvers/rateOfProgressOfSection.js
@@ -0,0 +1,46 @@
+const resolvers = {
+  RateOfProgressOfSection: {
+    id(rateOfProgressOfSection) {
+      return rateOfProgressOfSection._id
+    },
+
+    user(rateOfProgressOfSection, args, { RateOfProgressOfSection }) {
+      return RateOfProgressOfSection.user(rateOfProgressOfSection)
+    },
+
+    section(rateOfProgressOfSection, args, { RateOfProgressOfSection }) {
+      return RateOfProgressOfSection.section(rateOfProgressOfSection)
+    },
+
+    count(rateOfProgressOfSection, args, { RateOfProgressOfSection }) {
+      return RateOfProgressOfSection.count(rateOfProgressOfSection)
+    }
+
+  },
+  Query: {
+    rateOfProgressOfSections(root, { skip, limit }, { RateOfProgressOfSection }) {
+      return RateOfProgressOfSection.all({ skip, limit })
+    },
+
+    rateOfProgressOfSection(root, { id }, { RateOfProgressOfSection }) {
+      return RateOfProgressOfSection.findOneById(id)
+    }
+  },
+  Mutation: {
+    async createRateOfProgressOfSection(root, { input }, { RateOfProgressOfSection, Patient }) {
+      const id = await RateOfProgressOfSection.insert(input)
+      return RateOfProgressOfSection.findOneById(id)
+    },
+
+    async updateRateOfProgressOfSection(root, { id, input }, { RateOfProgressOfSection }) {
+      await RateOfProgressOfSection.updateById(id, input)
+      return RateOfProgressOfSection.findOneById(id)
+    },
+
+    removeRateOfProgressOfSection(root, { id }, { RateOfProgressOfSection }) {
+      return RateOfProgressOfSection.removeById(id)
+    }
+  }
+}
+
+export default resolvers
--- resolvers/userAnswer.js
+++ resolvers/userAnswer.js
@@ -26,8 +26,20 @@ const resolvers = {
     }
   },
   Mutation: {
-    async createUserAnswer(root, { input }, { UserAnswer, Patient }) {
+    async createUserAnswer(root, { input }, { UserAnswer, Answer, Exercise, RateOfProgressOfSection }) {
+      const { userId } = input
       const id = await UserAnswer.insert(input)
+      const answer = await Answer.findOneById(input.answerId)
+      const exercise = await Exercise.findOneById(answer.exerciseId)
+      const { sectionId } = exercise
+      const rateOfProgressOfSection = await RateOfProgressOfSection.collection.findOne({ userId, sectionId })
+      let current = 1
+      if (rateOfProgressOfSection) {
+        current = rateOfProgressOfSection.current + 1
+        await RateOfProgressOfSection.updateById(rateOfProgressOfSection._id, { current })
+      } else {
+        await RateOfProgressOfSection.insert({ userId, sectionId, current })
+      }
       return UserAnswer.findOneById(id)
     },
 
