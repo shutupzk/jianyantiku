3c9aa06ed8927888c9b71e3fa3bc1923586e7a4a
康  查 <956468883@qq.com>
2017-09-28 18:37:30 +0800

    xiugai

--- resolvers/userAnswer.js
+++ resolvers/userAnswer.js
@@ -39,21 +39,22 @@ const resolvers = {
       const user = await User.findOneById(userId)
       let { score, scoreUsed } = user
       if (type === '01') {
-        const answers = await Answer.collection.find({ exerciseId }).toArray()
-        let answerIds = []
-        for (let doc of answers) {
-          answerIds.push(doc._id)
+        // const answers = await Answer.collection.find({ exerciseId }).toArray()
+        // let answerIds = []
+        // for (let doc of answers) {
+        //   answerIds.push(doc._id)
+        // }
+        // const count = await UserAnswer.collection.count({ answerId: { $in: answerIds } })
+        // console.log('count === ', count)
+        // if (count === 0) {
+        if (scoreUsed > score || score === scoreUsed) {
+          throw new Error('您的积分不足')
         }
-        const count = await UserAnswer.collection.count({ answerId: { $in: answerIds } })
-        if (count === 0) {
-          if (scoreUsed > score || score === scoreUsed) {
-            throw new Error('您的积分不足')
-          }
-          scoreUsed += 0.1
-        }
+        scoreUsed += 0.1
+        // }
       }
       const id = await UserAnswer.insert(input)
-      // User.updateById(userId, { scoreUsed })
+      User.updateById(userId, { scoreUsed })
       const { sectionId, num, examinationDifficultyId } = exercise
       const rateOfProgressOfSection = await RateOfProgressOfSection.collection.findOne({ userId, sectionId, examinationDifficultyId })
       let current = num
